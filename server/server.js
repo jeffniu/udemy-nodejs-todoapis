730e1f0b954d962d78cc9796c2d9e25a791bdb2d
Niu Guangyuan <u0705666@gmail.com>
2019-01-21 22:18:25 -0800

    add login api

--- server/server.js
+++ server/server.js
@@ -92,7 +92,7 @@ app.patch('/todos/:id', (req, res) => {
   }).catch((e)=>{res.status(400).send()})
 })
 
-//POST /users
+//POST /users //sign up api
 app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password'])
   var user = new User(body)
@@ -107,6 +107,17 @@ app.post('/users', (req, res) => {
   })
 })
 
+//POST /users/login {email, password}
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password'])
+  User.findByCredentials(body.email, body.password).then((user) => {
+      return user.generateAuthToken().then((token) => {
+        res.header('x-auth', token).send(user)
+      })
+  }).catch((e) => {
+      res.status(400).send(e);
+  })
+})
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user)
