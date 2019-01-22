8f55618df7c2e9bb161279d70228568ac4b8d435
Niu Guangyuan <u0705666@gmail.com>
2019-01-21 18:10:13 -0800

    add post user route

--- server/models/user.js
+++ server/models/user.js
@@ -1,8 +1,10 @@
 
 var mongoose = require('mongoose')
 const validator = require('validator')
+const jwt = require('jsonwebtoken')
+const _ = require('lodash')
 
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema({
   email: {
     type: String,
     minlength: 1,
@@ -30,5 +32,21 @@ var User = mongoose.model('User', {
     }
   }]
 })
+
+UserSchema.methods.toJSON = function() {
+  var userObject = this.toObject()
+  return _.pick(userObject, ['_id', 'email']);
+}
+
+UserSchema.methods.generateAuthToken = function () {
+  var access = 'auth';
+  var token = jwt.sign({_id: this._id.toHexString(), access}, 'abc123').toString()
+  this.tokens = this.tokens.concat([{access, token}])
+  return this.save().then(()=> {
+    return token
+  })
+}
+
+var User = mongoose.model('User', UserSchema)
 
 module.exports = {User};
--- server/server.js
+++ server/server.js
@@ -93,9 +93,17 @@ app.patch('/todos/:id', (req, res) => {
 
 //POST /users
 app.post('/users', (req, res) => {
-
-}, (e) => {
-  res.status(400).send(e)
+  var body = _.pick(req.body, ['email', 'password'])
+  var user = new User(body)
+  user.save().then(() => {
+    // res.send(user);
+    return user.generateAuthToken();
+  }).then((token) => {
+      res.header('x-auth', token).send(user)
+  }).catch((e) => {
+    console.log(e)
+    res.status(400).send(e)
+  })
 })
 
 app.listen(process.env.PORT, () => {
